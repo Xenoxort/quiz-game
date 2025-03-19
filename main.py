d00c55027c37847da8041bd67bbbe92652558ecf
Gergely Banyai <banyai.gergely@gmail.com>
2025-03-19 12:22:47 +0100

    The questions are randomly selected and removed from the remaining questions list. No modules or classes were created yet.

---
+++ main.py
@@ -0,0 +1,27 @@
+import random
+import data
+
+remaining_questions = data.question_data.copy()
+n_questions = len(data.question_data)
+
+now = 1
+game_on = True
+
+while game_on:
+    current_data = random.choice(remaining_questions)
+    current_question = current_data["text"]
+    current_answer = current_data["answer"]
+
+    question = input(f"Q.{now}: {current_question} (True/False)?: ").lower()
+
+    if question == "off":
+        game_on = False
+
+    remaining_questions.remove(current_data)
+    now += 1
+    print(len(remaining_questions))
+
+
+
+
+
