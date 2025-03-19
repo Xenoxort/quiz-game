5370a0558c35b319031da6b0f25819758a426552
Gergely Banyai <banyai.gergely@gmail.com>
2025-03-19 14:58:50 +0100

    The quiz game is completed, but no modules or classes were used yet.

--- main.py
+++ main.py
@@ -4,22 +4,38 @@ import data
 remaining_questions = data.question_data.copy()
 n_questions = len(data.question_data)
 
-now = 1
+now = 0
+score = 0
 game_on = True
 
 while game_on:
+    now += 1
     current_data = random.choice(remaining_questions)
     current_question = current_data["text"]
     current_answer = current_data["answer"]
 
-    question = input(f"Q.{now}: {current_question} (True/False)?: ").lower()
+    player_answer = input(f"Q.{now}: {current_question} (True/False)?: ")
 
-    if question == "off":
+    if player_answer == "off":
         game_on = False
+        print("Game over! Thanks for playing.")
 
-    remaining_questions.remove(current_data)
-    now += 1
-    print(len(remaining_questions))
+    else:
+        if player_answer.lower() == current_answer.lower():
+            print("You got it right!")
+            score += 1
+        else:
+            print("That's wrong.")
+
+        print(f"The correct answer was: {current_answer}.")
+        print(f"Your current score is: {score}/{now}")
+
+        remaining_questions.remove(current_data)
+
+        if len(remaining_questions) == 0:
+            game_on = False
+            print("Game over! Thanks for playing.")
+        #print(len(remaining_questions))
 
 
 
